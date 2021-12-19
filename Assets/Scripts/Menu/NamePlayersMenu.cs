7c782229a3c01be018f09310987ae5018a512ff2
slatinsky <slatinsky.adam@gmail.com>
2021-12-19 16:41:43 +0100

    PlayScene columns fixed in different resolutions

--- Assets/Scripts/Menu/NamePlayersMenu.cs
+++ Assets/Scripts/Menu/NamePlayersMenu.cs
@@ -14,7 +14,7 @@ public class NamePlayersMenu : MonoBehaviour
     public InputField inputField;
 
     public List<InputField> inputFields = new List<InputField>();
-    
+
 
     void Awake()
     {
@@ -31,55 +31,75 @@ public class NamePlayersMenu : MonoBehaviour
 
         GameObject canvas = GameObject.FindGameObjectWithTag("Canvas");
 
-        // zatial pre race
-        int offset = 80;
+        // race
         if (PlayerPrefs.GetString("mode") == "all")
         {
             for (int i = 0; i < carCount; i++)
             {
-
-                // transform.localPosition = Vector3.zero;
-                inputFields.Add(Instantiate(inputField, new Vector3(0,800-(i*offset+offset),0), Quaternion.identity, canvas.transform));            
-                if (Global.carNames.Count > i)
-                {
-                    inputFields[i].text = Global.carNames[i];
-                } else {
-                    inputFields[i].text = "Car " + i;
-                }
-            }
-        } else
-        {
-            int indexTeam1 = 0;
-            int indexTeam2 = 0;
-            for (int i = 0; i < carCount; i++)
-            {
-                string carName;
-                if (Global.carNames.Count > i)
-                {
-                    carName = Global.carNames[i];
-                } else {
-                    carName = "Car " + i;
-                }
-
-                if (i%2 == 0)
-                {
-                    inputFields.Add(Instantiate(inputField, new Vector3(300,800-(indexTeam1*offset+offset),0), Quaternion.identity, canvas.transform));
-                    indexTeam1++;
-                } else 
-                {
-                    inputFields.Add(Instantiate(inputField, new Vector3(700,800-(indexTeam2*offset+offset),0), Quaternion.identity, canvas.transform));
-                    indexTeam2++;
-                }
-
-                inputFields[i].text = carName;
+                GameObject inputFieldObj = Instantiate(inputField.gameObject, canvas.transform);
+                inputFieldObj.transform.position = calculatePosition(1, 0, i);
+                inputFields.Add(inputFieldObj.GetComponent<InputField>());
+                inputFields[i].text = generateCarName(i);
             }
         }
+        else
+        {
+            for (int i = 0; i < carCount; i++)
+            {
+                GameObject inputFieldObj = Instantiate(inputField.gameObject, canvas.transform);
+                inputFieldObj.transform.position = calculatePosition(2, i % 2, i);
+                inputFields.Add(inputFieldObj.GetComponent<InputField>());
+                inputFields[i].text = generateCarName(i);
+            }
+        }
+    }
+
+    // get name from Global.carNames if exists fron index
+    // else return "Car " + index
+    private string generateCarName(int index)
+    {
+        if (Global.carNames.Count > index)
+        {
+            return Global.carNames[index];
+        }
+        else
+        {
+            return "Car " + index;
+        }
+    }
+
+
+    private Vector2 calculatePosition(int numberOfColumns, int columnIndex, int rowIndex)
+    {
+        // set offset based on i
+        float paddingY = Screen.height / 5;
+        float paddingX = Screen.height / 3;
+
+        float offsetY = Mathf.Ceil(rowIndex / numberOfColumns) * Screen.height / 15 + paddingY;
+        float offsetX;
+
+        if (numberOfColumns == 1)
+        {
+            offsetX = 0;
+        }
+        else
+        {
+            if (columnIndex == 0)
+            {
+                offsetX = paddingX;
+            }
+            else
+            {
+                offsetX = -paddingX;
+            }
+        }
+        return new Vector2(Screen.width / 2 - offsetX, Screen.height - offsetY);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SubmitBtnClick()
@@ -93,7 +113,8 @@ public class NamePlayersMenu : MonoBehaviour
         if (PlayerPrefs.GetString("gameMode") == "autodrom")
         {
             SceneManager.LoadScene("AutodromScene");
-        } else 
+        }
+        else
         {
             SceneManager.LoadScene("RaceScene");
         }
@@ -110,8 +131,9 @@ public class NamePlayersMenu : MonoBehaviour
 
         if (PlayerPrefs.GetString("gameMode") == "autodrom")
         {
-            SceneManager.LoadScene("AutodromMenuScene");        
-        } else
+            SceneManager.LoadScene("AutodromMenuScene");
+        }
+        else
         {
             SceneManager.LoadScene("PlayScene");
         }
